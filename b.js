5d46b3c8ae3d9dcf892b5d80beca48b28177da57
Stiven <johnortega13579@gmail.com>
2023-10-19 21:39:57 -0500

    Ejercicio numeros mayor/menor

--- b.js
+++ b.js
@@ -1,15 +1,15 @@
-let n1 = prompt("Ingrese el primer digito")
-let n2 = prompt("Ingrese el segundo digito")
-let n3 = prompt("Ingrese el tercer digito")
+let n1 = parseInt(prompt("Ingrese el primer digito"))
+let n2 = parseInt(prompt("Ingrese el segundo digito"))
+let n3 = parseInt(prompt("Ingrese el tercer digito"))
 if (n1 == n2 && n2 == n3){
     alert("¡Los numero son iguales!")
 }else{
     let mayor;
-    if (n1 >= n2 && n1 > n3){
+    if (n1 >= n2 && n1 >= n3){
         mayor = n1
-    }else if(n2 >=n1 && n2 >= n3){
+    }else if(n2 >= n1 && n2 >= n3){
         mayor = n2
-    }else{
+    }else if(n3 >= n1 && n3 >= n2){
         mayor = n3
     }
     alert("El numero mayor es:" + "\n" + mayor)
